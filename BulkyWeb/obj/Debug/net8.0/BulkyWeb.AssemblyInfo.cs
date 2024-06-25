d6799a1ea90846534034c4b3a041d248b77f9754
Bikash Shrestha <stha.bikam99@gmail.com>
2024-06-25 13:52:24 +0545

    Passing cutsom data to the database

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15050a8698b0782eb3910d428fe0659b75d1b17d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
